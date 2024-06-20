a7e873b81f3fa53e93ca89efa5c831766037c3de
Russ Marshall <rmarshall31@proton.me>
2024-06-20 15:23:00 +0100

    add dockerhub image

--- exporter.py
+++ exporter.py
@@ -1,3 +1,5 @@
+import signal
+import sys
 from KitronikAirQualityControlHAT import KitronikBME688, KitronikOLED
 from prometheus_client import start_http_server, Gauge
 from time import sleep
@@ -17,29 +19,40 @@ class KitronikExporter:
         self.bme688.calcBaselines(self.oled)
 
     def update_metrics(self):
-        self.bme688.measureData()
+        try:
+            self.bme688.measureData()
 
-        self.oled.clear()
-        self.oled.displayText("Temperature:" + str(self.bme688.readTemperature()), 1)
-        self.oled.displayText("Pressure:" + str(self.bme688.readPressure()), 2)
-        self.oled.displayText("Humidity:"+  str(self.bme688.readHumidity()), 3)
-        self.oled.displayText("eCO2:" + str(self.bme688.readeCO2()), 4)
-        self.oled.displayText("Air Quality %:" + str(self.bme688.getAirQualityPercent()), 5)
-        self.oled.displayText("Air Quality Score:" + str(self.bme688.getAirQualityScore()), 6)
-        self.oled.show()
+            self.oled.clear()
+            self.oled.displayText(f'Temperature: {self.bme688.readTemperature()}', 1)
+            self.oled.displayText(f'Pressure: {self.bme688.readPressure()}', 2)
+            self.oled.displayText(f'Humidity: {self.bme688.readHumidity()}', 3)
+            self.oled.displayText(f'eCO2: {self.bme688.readeCO2()}', 4)
+            self.oled.displayText(f'Air Quality %: {self.bme688.getAirQualityPercent()}', 5)
+            self.oled.displayText(f'Air Quality Score: {self.bme688.getAirQualityScore()}', 6)
+            self.oled.show()
 
-        self.temperature_gauge.set(self.bme688.readTemperature())
-        self.pressure_gauge.set(self.bme688.readPressure())
-        self.humidity_gauge.set(self.bme688.readHumidity())
-        self.ec02_gauge.set(self.bme688.readeCO2())
-        self.air_quality_percent_gauge.set(self.bme688.getAirQualityPercent())
-        self.air_quality_score_gauge.set(self.bme688.getAirQualityScore())
+            self.temperature_gauge.set(self.bme688.readTemperature())
+            self.pressure_gauge.set(self.bme688.readPressure())
+            self.humidity_gauge.set(self.bme688.readHumidity())
+            self.ec02_gauge.set(self.bme688.readeCO2())
+            self.air_quality_percent_gauge.set(self.bme688.getAirQualityPercent())
+            self.air_quality_score_gauge.set(self.bme688.getAirQualityScore())
+        except Exception as e:
+            print(f'Error updating metrics: {e}')
+
+
+def signal_handler(sig, frame):
+    print('Shutting down exporter...')
+    sys.exit(0)
 
 
 if __name__ == '__main__':
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+
     exporter = KitronikExporter()
     start_http_server(8000)
-    print("Exporter running on port 8000")
+    print('Exporter running on port 8000')
 
     while True:
         exporter.update_metrics()
